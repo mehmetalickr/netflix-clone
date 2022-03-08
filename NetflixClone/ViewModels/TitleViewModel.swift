5642f6c062e16157b06efdbb96e2360df5aee44a
Mehmet Ali ÇAKIR <ckralimehmet@gmail.com>
2022-03-08 23:33:37 +0300

    Create and Update Upcoming Section

---
+++ NetflixClone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  NetflixClone
+//
+//  Created by Mehmet Ali ÇAKIR on 8.03.2022.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
